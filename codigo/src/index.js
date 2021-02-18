a65c99ee320fa5524d65bb9d5ecf3e4070a876fe
acalderonl1 <acalderonl1@est.ups.edu.ec>
2021-02-18 10:47:29 -0500

    levantado el servidor y estructura general de Hopark

---
+++ codigo/src/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const morgan = require('morgan')
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+
+//inicializacion
+const app = express();
+
+
+//configuracion
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
+
+
+
+//funciones de peticiones
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+
+//variables globales
+app.use((req, res, next)=>{
+    next();
+});
+
+
+
+//rutas
+app.use(require('./routes'))
+app.use(require('./routes/authentication'))
+app.use('/link', require('./routes/links'))
+
+
+//public
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+//iniciar servidor
+app.listen(app.get('port'), ()=>{
+    console.log('el servidor esta escuchando por el puerto', app.get('port'));
+});
